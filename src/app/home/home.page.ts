6f3566faaaa5362a17da5e24ac3ab6b2aa3f249f
edison <eddytiger7@gmail.com>
2024-10-25 11:53:59 +0200

    Adding the colour picker component to be able to pick the colour to be updated on the pie chart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonRouterOutlet } from '@ionic/angular/standalone';
 import { PieChartComponent } from "../pie-chart/pie-chart.component";
 import { ColourPickerComponent } from '../colour-picker/colour-picker.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, PieChartComponent, ColourPickerComponent],
+  imports: [IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, PieChartComponent, ColourPickerComponent, CommonModule, FormsModule],
 })
 export class HomePage {
+  showColorPicker: boolean = false;
+  buttonText: string = 'Show color picker'
+
   constructor() {}
+
+  toggleColorPicker(): void{
+    this.showColorPicker = !this.showColorPicker;
+
+    if(this.showColorPicker) {
+      this.buttonText = 'Hide color picker';
+    }
+    else{
+      this.buttonText = 'Show color picker';
+    }
+  }
 }
