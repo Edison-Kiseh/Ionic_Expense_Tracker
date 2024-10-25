681b4faf1caf749e67a8149b99114d468c8693f9
edison <eddytiger7@gmail.com>
2024-10-25 11:22:48 +0200

    Color select init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonRouterOutlet } from '@ionic/angular/standalone';
 import { PieChartComponent } from "../pie-chart/pie-chart.component";
+import { ColourPickerComponent } from '../colour-picker/colour-picker.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent,  PieChartComponent],
+  imports: [IonRouterOutlet, IonHeader, IonToolbar, IonTitle, IonContent, PieChartComponent, ColourPickerComponent],
 })
 export class HomePage {
   constructor() {}
